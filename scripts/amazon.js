c3d5c21bdb5875565a276af6a95af36ce5bd5822
hjchen0428 <huang_jian88@126.com>
2024-02-03 20:20:10 +0800

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -116,6 +116,14 @@ addToCartButtons.forEach((button) => {
 				quantity: 1,
 			});
 		}
-		console.log(cart);
+
+		let cartQuantity = 0;
+
+		cart.forEach((item) => {
+			cartQuantity += item.quantity;
+		});
+
+		document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
 	});
 });
